a84e7d482738c43eb8f31416097288c8de30f550
et422 <euan.turner22@imperial.ic.ac.uk>
2025-09-12 08:39:34 +0100

    loss gradient tests

--- test/loss/loss_test.cpp
+++ test/loss/loss_test.cpp
@@ -40,3 +40,48 @@ TEST_CASE("MSE loss computes correct value for simple case", "[loss][mse]") {
     float expected = 0.5f * ((1.0f-0.8f)*(1.0f-0.8f) + (0.0f-0.2f)*(0.0f-0.2f));
     REQUIRE(loss == Catch::Approx(expected));
 }
+
+
+TEST_CASE("CCE loss backward computes correct gradient for simple case", "[loss][cce][backward]") {
+    Tensor y_true = Tensor::vector(3);
+    y_true.set(0, 0.0f); y_true.set(1, 1.0f); y_true.set(2, 0.0f);
+    Tensor y_pred = Tensor::vector(3);
+    y_pred.set(0, 0.1f); y_pred.set(1, 0.8f); y_pred.set(2, 0.1f);
+    loss::CCE cce;
+    cce.forward(y_pred, y_true);
+    Tensor grad = cce.backward();
+    // Gradient should be -y_true / y_pred
+    REQUIRE(grad(0) == Catch::Approx(0.0f));
+    REQUIRE(grad(1) == Catch::Approx(-1.0f / 0.8f));
+    REQUIRE(grad(2) == Catch::Approx(0.0f));
+}
+
+TEST_CASE("BCE loss backward computes correct gradient for simple case", "[loss][bce][backward]") {
+    Tensor y_true = Tensor::vector(2);
+    y_true.set(0, 1.0f); y_true.set(1, 0.0f);
+    Tensor y_pred = Tensor::vector(2);
+    y_pred.set(0, 0.9f); y_pred.set(1, 0.1f);
+    loss::BCE bce;
+    bce.forward(y_pred, y_true);
+    Tensor grad = bce.backward();
+    // Gradient: - (y_true / y_pred - (1-y_true)/(1-y_pred)) / N
+    float g0 = - (1.0f / 0.9f - 0.0f / (1.0f - 0.9f)) / 2.0f;
+    float g1 = - (0.0f / 0.1f - 1.0f / (1.0f - 0.1f)) / 2.0f;
+    REQUIRE(grad(0) == Catch::Approx(g0));
+    REQUIRE(grad(1) == Catch::Approx(g1));
+}
+
+TEST_CASE("MSE loss backward computes correct gradient for simple case", "[loss][mse][backward]") {
+    Tensor y_true = Tensor::vector(2);
+    y_true.set(0, 1.0f); y_true.set(1, 0.0f);
+    Tensor y_pred = Tensor::vector(2);
+    y_pred.set(0, 0.8f); y_pred.set(1, 0.2f);
+    loss::MSE mse;
+    mse.forward(y_pred, y_true);
+    Tensor grad = mse.backward();
+    // Gradient: 2/N * (y_pred - y_true)
+    float g0 = 2.0f/2.0f * (0.8f - 1.0f);
+    float g1 = 2.0f/2.0f * (0.2f - 0.0f);
+    REQUIRE(grad(0) == Catch::Approx(g0));
+    REQUIRE(grad(1) == Catch::Approx(g1));
+}
